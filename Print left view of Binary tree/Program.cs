b65334e2d70211fb0e4daaee2ea36ee4296953d4
Chinmay <Chinmay8116@gmail.com>
2021-06-22 06:44:20 +0530

    commit-leftview

---
+++ Print left view of Binary tree/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Ttree_Traversals;
+
+
+namespace Print_left_view_of_Binary_tree
+{
+    public static class AppHelper
+    {
+        static int maxlevel = 0;
+        public static void LeftView(BinaryTree root,int level)
+        {
+            if (root == null)
+                return;
+            if(maxlevel <level)
+            {
+                Console.WriteLine(root.key);
+                maxlevel = level;
+            }
+            LeftView(root.left,level + 1);
+            LeftView(root.left, level + 1);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
