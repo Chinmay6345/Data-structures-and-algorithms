22b187d6dd15522f3108400bf7009952bf8eeda6
Chinmay <Chinmay8116@gmail.com>
2021-06-28 10:12:44 +0530

    commit -iterative pre order

--- Iterative Pre-order traversal/Program.cs
+++ Iterative Pre-order traversal/Program.cs
@@ -2,27 +2,25 @@
 using System.Linq;
 using System.Collections.Generic;
 using Count_nodes_in_a_complete_Binary_Tree;
-namespace Iterative_In_order_traversal
+
+namespace Iterative_Pre_order_traversal
 {
     public static class AppHelper
     {
         public static void IterativeInorder(TreeNode root)
         {
             Stack<TreeNode> stack = new Stack<TreeNode>();
-            stack.Push(root);
-            while(stack.Any())
+            TreeNode curr = root;
+            while(curr!=null || stack.Any())
             {
-                TreeNode curr = stack.Peek();
+                while(curr!=null)
+                {
+                    stack.Push(curr);
+                    curr = curr.left;
+                }
+                curr = stack.Pop();
                 Console.Write(curr.val + " ");
-                stack.Pop();
-                if(curr.left!=null)
-                {
-                   stack.Push(curr.left);
-                }
-                if(curr.right!=null)
-                {
-                    stack.Push(curr.right);
-                }
+                curr = curr.right;
             }
         }
     }
